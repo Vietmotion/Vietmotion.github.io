f2e4b873c2007fee13f17c28a0caf9333b02fc77
VizzleLe <104678057+Vietmotion@users.noreply.github.com>
2025-04-13 18:52:52 +0700

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,12 @@
+function updateClock() {
+  const now = new Date();
+  const timeString = now.toLocaleTimeString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  });
+  document.getElementById("clock").textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
+updateClock(); // run once immediately
